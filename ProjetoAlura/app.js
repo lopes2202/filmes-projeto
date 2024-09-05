a988a4c06065b42a062e1387454c462e60249def
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-04 21:16:20 -0300

    projeto alura

---
+++ ProjetoAlura/app.js
@@ -0,0 +1,2 @@
+
+console.log(filmesDados[1].descricao)
---
+++ ProjetoAlura/dados.js
@@ -0,0 +1,20 @@
+let filmesDados = [
+    {
+        nome: "Até o Último Homem",
+        descricao: "Só mais um. \nNo longa \"Até o Último Homem\", acompanhamos a trajetória do Soldado Desmond Doss, um cara comum, filho de ex militar e adventista, que decide lutar na Segunda Guerra Mundial, mas com um porém, sem encostar em armas ou tirar vidas humanas. O filme surpreende desde o início, com uma fotografia linda que leva o telespectador a se emocionar. O que mais me surpreendeu e, honestamente, me deixou mais surpresa sobre o filme, é que foi baseado em uma história real, fazendo com que todos os acontecimentos da trama que fizeram arrepiar, me deixassem cinquenta vezes mais emocionada. \"Até o Último Homem\" foi dirigido pelo renomado Mel Gibson, diretor também de obras como Mad Max e A Paixão de Cristo, e protagonizado pelo eterno Espetacular Homem-Aranha, Andrew Garfield, que realmente deu um espetáculo de atuação. O longa foi tão aclamado que foi indicado a diversos Oscars, dentre eles, Melhor Filme de 2016, entre outras premiações.",
+        anoqueLancou: 2016,
+        link: "https://www.youtube.com/embed/s2-1hz1juBI"
+    },
+    {
+        nome: "Como Perder um Homem em 10 Dias",
+        descricao: "Você não tem como perder alguém que nunca teve! \nComo Perder um Homem em 10 Dias é o clássico que nunca sai de moda, o clássico filme para se assistir em uma tarde com as amigas, com o namorado ou até mesmo sozinha, comendo um brigadeiro de colher e sofrendo por um cara que definitivamente não te merece.\n\nA trama acompanha a história de Andy, uma jornalista de uma revista de moda, que deseja escrever sobre temas importantes para a sociedade, e não sobre 'Como se livrar de uma multa de trânsito'. Para isso, ela começa um novo e último artigo, ensinar garotas a como não agir em um novo relacionamento.\n\nJá Ben, é um publicitário sedutor que diz conhecer as mulheres, para isso, ele aposta com duas colegas que consegue fazer qualquer mulher se apaixonar em 10 dias.\n\nParece óbvio, não?! Um fazendo de tudo para conquistar o coração da garota e outra fazendo de tudo para afastar o cara. O que isso poderia dar?",
+        anoqueLancou: 2003,
+        link: "https://www.youtube.com/embed/3IDeifYOCvM"
+    },
+    {
+        nome: "The Notebook",
+        descricao: "",
+        anoqueLancou: 2004,
+        link: "https://www.youtube.com/embed/BjJcYdEOI0k"
+    }
+];
