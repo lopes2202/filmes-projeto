46a9141e5fdd9cec10b200a80c00459173862c0e
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-06 19:55:49 -0300

    finalizando projeto

--- ProjetoAlura/app.js
+++ ProjetoAlura/app.js
@@ -1,25 +1,46 @@
 function pesquisar() {
-    // Seleciona a seção onde os resultados da pesquisa serão exibidos.
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
-    let resultados = "";
-  
-    // Itera sobre cada filme no array 'filmesDados'.
-    for (let dado of filmesDados) {
-      // Cria uma div para cada filme, formatando-a como um resultado de pesquisa.
-      resultados += `<div class="item-resultado">
-        <h2>
-          <a href="#" target="_blank">${dado.nome}</a>
-        </h2>
-        <img src=${dado.imagem} alt="Descrição da imagem" class="img1">
-        <p class="descricao-meta">${dado.descricao} </p>
-        <div class="video-container">
-          <iframe width="560" height="315" src=${dado.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-      </div>`;
-    }
-  
-    // Atribui o conteúdo HTML gerado à seção de resultados.
-    section.innerHTML = resultados;
+  // Seleciona a seção onde os resultados da pesquisa serão exibidos.
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+  if (campoPesquisa == "") {
+    section.innerHTML = "Nada foi encontrado. Você precisa digitar corretamente o nome do filme ou gênero."
+    return
   }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
+  let resultados = "";
+  let nome = "";
+  let genero = ""
+
+
+  // Itera sobre cada filme no array 'filmesDados'.
+  for (let dado of filmesDados) {
+    nome = dado.nome.toLowerCase();
+    genero = dado.genero.toLowerCase();
+    if (nome.includes(campoPesquisa) || genero.includes(campoPesquisa)) {
+      resultados += `<div class="item-resultado">
+      <h2>
+        <a href="#" target="_blank">${dado.nome}</a>
+      </h2>
+      <img src=${dado.imagem} alt="Descrição da imagem" class="img1">
+      <p class="descricao-meta">${dado.descricao} </p>
+      <div class="video-container">
+        <iframe width="560" height="315" src=${dado.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>`;
+    }
+
+    // Cria uma div para cada filme, formatando-a como um resultado de pesquisa.
+  }
+
+  if(resultados == ""){
+    section.innerHTML = "Filme não encontrado."
+    return
+  }
+  // Atribui o conteúdo HTML gerado à seção de resultados.
+  section.innerHTML = resultados;
+}
--- ProjetoAlura/dados.js
+++ ProjetoAlura/dados.js
@@ -1,7 +1,8 @@
 let filmesDados = [
     {
         nome: "Até o Último Homem",
-        descricao: "Só mais um. \nNo longa \"Até o Último Homem\", acompanhamos a trajetória do Soldado Desmond Doss, um cara comum, filho de ex militar e adventista, que decide lutar na Segunda Guerra Mundial, mas com um porém, sem encostar em armas ou tirar vidas humanas. O filme surpreende desde o início, com uma fotografia linda que leva o telespectador a se emocionar. O que mais me surpreendeu e, honestamente, me deixou mais surpresa sobre o filme, é que foi baseado em uma história real, fazendo com que todos os acontecimentos da trama que fizeram arrepiar, me deixassem cinquenta vezes mais emocionada. \"Até o Último Homem\" foi dirigido pelo renomado Mel Gibson, diretor também de obras como Mad Max e A Paixão de Cristo, e protagonizado pelo eterno Espetacular Homem-Aranha, Andrew Garfield, que realmente deu um espetáculo de atuação. O longa foi tão aclamado que foi indicado a diversos Oscars, dentre eles, Melhor Filme de 2016, entre outras premiações.",
+        descricao: "Só mais um. \n No longa \"Até o Último Homem\", acompanhamos a trajetória do Soldado Desmond Doss, um cara comum, filho de ex militar e adventista, que decide lutar na Segunda Guerra Mundial, mas com um porém, sem encostar em armas ou tirar vidas humanas. O filme surpreende desde o início, com uma fotografia linda que leva o telespectador a se emocionar. O que mais me surpreendeu e, honestamente, me deixou mais surpresa sobre o filme, é que foi baseado em uma história real, fazendo com que todos os acontecimentos da trama que fizeram arrepiar, me deixassem cinquenta vezes mais emocionada. \"Até o Último Homem\" foi dirigido pelo renomado Mel Gibson, diretor também de obras como Mad Max e A Paixão de Cristo, e protagonizado pelo eterno Espetacular Homem-Aranha, Andrew Garfield, que realmente deu um espetáculo de atuação. O longa foi tão aclamado que foi indicado a diversos Oscars, dentre eles, Melhor Filme de 2016, entre outras premiações.",
+        genero: "guerra drama ficção ",
         anoqueLancou: 2016,
         imagem: "imagem1.jpg",
         link: "https://www.youtube.com/embed/s2-1hz1juBI"
@@ -9,14 +10,16 @@ let filmesDados = [
     {
         nome: "Como Perder um Homem em 10 Dias",
         descricao: "Você não tem como perder alguém que nunca teve! \nComo Perder um Homem em 10 Dias é o clássico que nunca sai de moda, o clássico filme para se assistir em uma tarde com as amigas, com o namorado ou até mesmo sozinha, comendo um brigadeiro de colher e sofrendo por um cara que definitivamente não te merece.\n\nA trama acompanha a história de Andy, uma jornalista de uma revista de moda, que deseja escrever sobre temas importantes para a sociedade, e não sobre 'Como se livrar de uma multa de trânsito'. Para isso, ela começa um novo e último artigo, ensinar garotas a como não agir em um novo relacionamento.\n\nJá Ben, é um publicitário sedutor que diz conhecer as mulheres, para isso, ele aposta com duas colegas que consegue fazer qualquer mulher se apaixonar em 10 dias.\n\nParece óbvio, não?! Um fazendo de tudo para conquistar o coração da garota e outra fazendo de tudo para afastar o cara. O que isso poderia dar?",
+        genero: "comédia romance",
         imagem: "imagem2.jpg",
         anoqueLancou: 2003,
         link: "https://www.youtube.com/embed/3IDeifYOCvM"
     },
     {
         nome: "The Notebook",
-        descricao: "",
-        imagem: "",
+        descricao: "A trama se desenrola em duas linhas temporais: o presente e o passado. No presente, acompanhamos um idoso lendo um diário para uma senhora em uma casa de repouso. Essa história, contada em detalhes vívidos, nos transporta para a década de 1940, onde conhecemos Noah Calhoun, um jovem operário, e Allie Hamilton, uma jovem rica. Os dois se apaixonam perdidamente, mas a diferença social e a Segunda Guerra Mundial os separam.",
+        genero: "drama romance comédia",
+        imagem: "imagem3.jpg",
         anoqueLancou: 2004,
         link: "https://www.youtube.com/embed/BjJcYdEOI0k"
     }
